0b3761034febea8bcd8199255d0b4bfc8b3994bd
joeltalemwa2-hash <jtalemwa@byupathway.edu>
2026-03-12 17:56:07 +0300

    m

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your first name? ");
+        Console.WriteLine("What is your first name?   ");
         string firstName = Console.ReadLine();
 
         Console.Write("What is your last name? ");
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,64 @@
 using System;
-
+// EXCEEDING REQUIREMENTS
+// Added additional prompts and improved display formatting.
+// Also clears journal before loading to prevent duplicate entries.
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+         Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        int choice = 0;
+
+        while (choice != 5)
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Load");
+            Console.WriteLine("5. Quit");
+
+            Console.Write("What would you like to do? ");
+            choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+
+                Console.WriteLine(prompt);
+                Console.Write("> ");
+                string response = Console.ReadLine();
+
+                string date = DateTime.Now.ToShortDateString();
+
+                Entry newEntry = new Entry();
+                newEntry._date = date;
+                newEntry._promptText = prompt;
+                newEntry._entryText = response;
+
+                journal.AddEntry(newEntry);
+            }
+
+            else if (choice == 2)
+            {
+                journal.DisplayAll();
+            }
+
+            else if (choice == 3)
+            {
+                Console.Write("Enter filename: ");
+                string file = Console.ReadLine();
+                journal.SaveToFile(file);
+            }
+
+            else if (choice == 4)
+            {
+                Console.Write("Enter filename: ");
+                string file = Console.ReadLine();
+                journal.LoadFromFile(file);
+            }
+        }
     }
 }
